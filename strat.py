dcbdd787566a7bad720f8a3967f5462e55d3c904
Ray C <hacker4street@gmail.com>
2023-08-11 17:00:53 +0200

    brought modules over to strat

--- strat.py
+++ strat.py
@@ -1,4 +1,13 @@
-from modules import *
+from keys import API_KEY, SECRET_KEY
+import datetime as dt
+import pandas as pd
+import numpy as np
+import talib as ta
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import MarketOrderRequest
+from alpaca.trading.enums import TimeInForce, OrderSide
+from alpaca.data.requests import StockBarsRequest, TimeFrame, DataFeed
+from alpaca.data.historical import StockHistoricalDataClient
 
 today = dt.date.today()
 start_date = today - dt.timedelta(days=300)
